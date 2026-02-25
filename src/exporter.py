af94028c2af8b2be3996e56f56f85889757df911
Felipe Santos <ofelipedsnts@gmail.com>
2026-02-24 21:29:51 -0300

    feat: finalizando funções do exporter

--- src/exporter.py
+++ src/exporter.py
@@ -18,3 +18,32 @@ def export_data_by_orgao(df:pd.DataFrame):
     df.to_csv(output_path, index=False, encoding="utf-8")
 
     return print(f"Arquivo exportado com sucesso {output_path}")
+    
+
+def export_saques_dataframe(df:pd.DataFrame):
+    saques = (df["transacao"] == "SAQUE - INT$ - APRES") | (df["transacao"] == "SAQUE CASH/ATM BB")
+    df = df[saques]
+
+    output_path = Path().resolve().parent / "data" / "processed" / "transacoes_de_saque.csv"
+    df.to_csv(output_path, index=False, encoding="utf-8")
+
+    return print(f"[export_saques]: Arquivo exportado com sucesso {output_path}")
+
+
+def export_data_by_servidor(df:pd.DataFrame):
+    new_df = (
+    df.groupby(by=["nome_portador"], as_index=False)
+        .agg({
+            "valor_transacao": ["sum", "mean", "count"],
+        })
+    )
+
+    new_df.columns = ["Nome do servidor", "Gastos totais", "Valor médio por transação", "Quantidade de transações"]
+
+    new_df["Gastos totais"] = new_df["Gastos totais"].round(2)
+    new_df["Valor médio por transação"] = new_df["Valor médio por transação"].round(2) 
+
+    output_path = Path().resolve().parent / "data" / "processed" / "resumo_servidores.csv"
+    df.to_csv(output_path, index=False, encoding="utf-8")
+
+    return print(f"[export_data_by_servidor]: Arquivo exportado com sucesso {output_path}")
